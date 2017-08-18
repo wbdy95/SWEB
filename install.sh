c0aad428a7d7ac18e17d36ce9b1e72a81ade821d
wbdy95 <951964022@qq.com>
2017-08-18 22:15:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ read -p "Please input your web password：" webpasswd
 
 
 #Install SSR (Powered By Teddysun : https://shadowsocks.be/9.html)
-wget -N --no-check-certificate https://raw.githubusercontent.com/FunctionClub/shadowsocks_install/master/shadowsocksR.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/wbdy95/shadowsocks_install/master/shadowsocksR.sh
 chmod +x shadowsocksR.sh
 bash shadowsocksR.sh
 rm -rf shadowsocksR.sh
@@ -73,7 +73,7 @@ rm -rf caddy_install.sh
 
 #Install SWEB
 cd /usr/local/
-git clone https://github.com/FunctionClub/SWEB
+git clone https://github.com/wbdy95/SWEB
 chmod +x /usr/local/SWEB/cgi-bin
 
 #Configure Caddy Proxy
@@ -84,7 +84,7 @@ echo ":80 {
 service caddy restart
 
 #Download SWEB Manager
-wget -N --no-check-certificate -O /usr/local/bin/sweb https://raw.githubusercontent.com/FunctionClub/SWEB/master/sweb
+wget -N --no-check-certificate -O /usr/local/bin/sweb https://raw.githubusercontent.com/wbdy95/SWEB/master/sweb
 chmod +x /usr/local/bin/sweb
 
 #Start SWEB in Screen
